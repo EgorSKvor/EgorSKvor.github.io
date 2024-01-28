f8d96a4ce1b61d7f1ec12625c20d6f9ca285aa95
Egor <ee3068316@gmail.com>
2024-01-28 15:32:46 +0300

    v2 tg_web_app

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.utils import executor
 from token_1 import TOKEN
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types.web_app_info import WebAppInfo
 
 
 bot = Bot(token=TOKEN)
-
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 author_name = 'Егор'
 
 
-@dp.message_handler(commands=['start'])
-async def hello_func(message: Message):
-    print(message.message_id, message.text)
-    await message.answer(text=f'Привет, {message.chat.username}\nТы можешь ознакомиться с личным сайтом {author_name}а по ссылке')
+# @dp.message_handler()
+# async def hello_func(message: Message):
+#     print(message.message_id, message.text)
+#     await message.answer(text=f'Привет, {message.chat.username}\nТы можешь ознакомиться с личным сайтом {author_name}а по ссылке')
 
-executor.start_polling(dispatcher=dp)
+
+@dp.message_handler(commands='start')
+async def start_web_app(message: Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton(
+        text='Открыть страницу анкеты', web_app=WebAppInfo(url='https://egorskvor.github.io')))
+    await message.answer(text='Жми кнопку', reply_markup=markup)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
